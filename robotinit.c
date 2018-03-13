91441bded9960a2387d4691843043d9eb9a4cd1e
Prithwiraj Bose <sribasu@gmail.com>
2018-03-13 19:54:26 +0530

    Update robotinit.c

--- robotinit.c
+++ robotinit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 main() {
-system("/opt/app-root/src/a.sh");
+  printf("Starting Jenkins on server...");
+system("robot -d result Tests/Build_Acceptance_Script/Build_Acceptance_Script.robot");
 }
